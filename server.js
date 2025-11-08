8aee0fed9564bc3265831bd8006c35ec493a330f
shivam79315 <158444124+shivam79315@users.noreply.github.com>
2025-11-08 22:23:29 +0530

    Updated Swagger api

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ import "./env.js";
 import express from 'express';
 import swagger from 'swagger-ui-express';
 import cors from 'cors';
+import fs from 'fs';
 
 
 import productRouter from './src/features/product/product.routes.js';
 import userRouter from './src/features/user/user.routes.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import cartRouter from './src/features/cartItems/cartItems.routes.js';
-import apiDocs from './swagger.json' assert {type: 'json'};
 import loggerMiddleware from './src/middlewares/logger.middleware.js';
+
 import { ApplicationError } from './src/error-handler/applicationError.js';
 import {connectToMongoDB} from './src/config/mongodb.js';
 // 2. Create Server
@@ -23,8 +24,11 @@ const server = express();
 // CORS policy configuration
 
 var corsOptions = {
-  origin: "http://localhost:5500"
-}
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
+};
+
 server.use(cors(corsOptions));
 
 // server.use((req, res, next)=>{
@@ -42,6 +46,10 @@ server.use(express.json());
 // Bearer <token>
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/products
+
+
+const apiDocs = JSON.parse(fs.readFileSync('./swagger.json', 'utf-8'));
+
 server.use("/api-docs", 
 swagger.serve, 
 swagger.setup(apiDocs)
