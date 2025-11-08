3c68236f584900fa6266f4ba883f87ed3ce3765c
shivam79315 <158444124+shivam79315@users.noreply.github.com>
2025-11-09 01:35:41 +0530

    File Upload

--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -3,7 +3,7 @@
 // 1. Import express.
 import express from 'express';
 import ProductController from './product.controller.js';
-import { uploadToCloudinary } from '../../middlewares/fileupload.middleware.js'
+import { uploadToCloudinary } from '../../middlewares/fileUpload.middleware.js'
 import jwtAuth from '../../middlewares/jwt.middleware.js';
 
 // 2. Initialize Express router.
--- src/middlewares/fileUpload.middleware.js
+++ src/middlewares/fileUpload.middleware.js
@@ -1,4 +1,4 @@
-// middlewares/fileupload.middleware.js
+// middlewares/fileUpload.middleware.js
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 
